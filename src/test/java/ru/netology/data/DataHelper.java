d819e43998a27f072321c26d8d7f0b560a50d324
Dilya test <dilya.yusufova@gmail.com>
2026-09-03 10:46:14 -0700

    Fix tests according to review

---
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -0,0 +1,28 @@
+package ru.netology.data;
+
+public class DataHelper {
+
+    public static String getLogin() {
+        return "vasya";
+    }
+
+    public static String getPassword() {
+        return "qwerty123";
+    }
+
+    public static String getVerificationCode() {
+        return "12345";
+    }
+
+    public static String getFirstCardNumber() {
+        return "5559 0000 0000 0001";
+    }
+
+    public static String getSecondCardNumber() {
+        return "5559 0000 0000 0002";
+    }
+
+    public static int getTransferAmount() {
+        return 200;
+    }
+}
--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -1,49 +1,27 @@
 package ru.netology.test;
 
 import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
 import ru.netology.pages.DashboardPage;
 import ru.netology.pages.LoginPage;
 import ru.netology.pages.TransferPage;
 import ru.netology.pages.VerificationPage;
-import org.junit.jupiter.api.Disabled;
-
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class MoneyTransferTest {
 
-    @BeforeEach
-    void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-
-        if (Boolean.parseBoolean(System.getProperty("selenide.headless", "false"))) {
-            options.addArguments("--headless=new");
-        }
-
-        Map<String, Object> prefs = new HashMap<>();
-        prefs.put("credentials_enable_service", false);
-        prefs.put("password_manager_enabled", false);
-        prefs.put("profile.password_manager_leak_detection", false);
-
-        options.setExperimentalOption("prefs", prefs);
-        Configuration.browserCapabilities = options;
-    }
-
     private DashboardPage loginAndOpenDashboard() {
         open("http://localhost:9999");
 
         LoginPage loginPage = new LoginPage();
-        loginPage.login("vasya", "qwerty123");
+        loginPage.login(
+                DataHelper.getLogin(),
+                DataHelper.getPassword()
+        );
 
         VerificationPage verificationPage = new VerificationPage();
-        verificationPage.verify("12345");
+        verificationPage.verify(DataHelper.getVerificationCode());
 
         DashboardPage dashboardPage = new DashboardPage();
         dashboardPage.verifyPage();
@@ -58,12 +36,15 @@ public class MoneyTransferTest {
         int firstCardBalance = dashboardPage.getFirstCardBalance();
         int secondCardBalance = dashboardPage.getSecondCardBalance();
 
-        int amount = 200;
+        int amount = DataHelper.getTransferAmount();
 
         dashboardPage.topUpFirstCard();
 
         TransferPage transferPage = new TransferPage();
-        transferPage.transferMoney(amount, "5559 0000 0000 0002");
+        transferPage.transferMoney(
+                amount,
+                DataHelper.getSecondCardNumber()
+        );
 
         int firstCardBalanceAfterTransfer =
                 dashboardPage.getFirstCardBalance();
@@ -89,12 +70,15 @@ public class MoneyTransferTest {
         int firstCardBalance = dashboardPage.getFirstCardBalance();
         int secondCardBalance = dashboardPage.getSecondCardBalance();
 
-        int amount = 200;
+        int amount = DataHelper.getTransferAmount();
 
         dashboardPage.topUpSecondCard();
 
         TransferPage transferPage = new TransferPage();
-        transferPage.transferMoney(amount, "5559 0000 0000 0001");
+        transferPage.transferMoney(
+                amount,
+                DataHelper.getFirstCardNumber()
+        );
 
         int firstCardBalanceAfterTransfer =
                 dashboardPage.getFirstCardBalance();
@@ -109,38 +93,6 @@ public class MoneyTransferTest {
 
         org.junit.jupiter.api.Assertions.assertEquals(
                 secondCardBalance + amount,
-                secondCardBalanceAfterTransfer
-        );
-    }
-
-    @Disabled("Known bug: transfer is allowed when amount exceeds card balance")
-    @Test
-    void shouldNotTransferMoreThanCardBalance() {
-        DashboardPage dashboardPage = loginAndOpenDashboard();
-
-        int firstCardBalance = dashboardPage.getFirstCardBalance();
-        int secondCardBalance = dashboardPage.getSecondCardBalance();
-
-        int amount = secondCardBalance + 1000;
-
-        dashboardPage.topUpFirstCard();
-
-        TransferPage transferPage = new TransferPage();
-        transferPage.transferMoney(amount, "5559 0000 0000 0002");
-
-        int firstCardBalanceAfterTransfer =
-                dashboardPage.getFirstCardBalance();
-
-        int secondCardBalanceAfterTransfer =
-                dashboardPage.getSecondCardBalance();
-
-        org.junit.jupiter.api.Assertions.assertEquals(
-                firstCardBalance,
-                firstCardBalanceAfterTransfer
-        );
-
-        org.junit.jupiter.api.Assertions.assertEquals(
-                secondCardBalance,
                 secondCardBalanceAfterTransfer
         );
     }
